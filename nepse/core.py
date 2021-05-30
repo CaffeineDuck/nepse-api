37439a37f178b185b9091fb84769c3628623f874
Samrid Pandit <samrid.pandit@gmail.com>
2021-05-31 00:09:48 +0545

    Added typehint to the return value of get_company

--- nepse/core.py
+++ nepse/core.py
@@ -1,5 +1,4 @@
-import asyncio
-from contextlib import suppress
+from nepse.security.types import SecurityResponse
 from typing import Optional
 
 import aiohttp
@@ -12,9 +11,11 @@ class Client:
         self._session = aiohttp.ClientSession()
         self._security_client = SecurityClient(self._session)
 
-    async def get_company(self, id: Optional[int] = None, symbol: Optional[str] = None):
+    async def get_company(
+        self, id: Optional[int] = None, symbol: Optional[str] = None
+    ) -> SecurityResponse:
         model = await self._security_client.get_security_response(id, symbol)
         return model
 
     async def close(self) -> None:
-       await self._session.close()
+        await self._session.close()
