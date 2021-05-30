1234648a6aba2cab4d131002c23eaf013fb22f66
Samrid Pandit <samrid.pandit@gmail.com>
2021-05-30 21:19:24 +0545

    Added Examples

--- examples/basic_highest_price.py
+++ examples/basic_highest_price.py
@@ -6,6 +6,7 @@ from nepse.core import Client
 async def main():
     client = Client()
     data = await client.get_company(symbol="UPPER")
+    print(data.daily_trade.high_price)
     await client.close()
 
 asyncio.run(main())
