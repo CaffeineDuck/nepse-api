e3e336746c036a7aee9e9dd479f9d45f2a5c6c46
Samrid Pandit <samrid.pandit@gmail.com>
2021-05-31 23:55:34 +0545

    Used httpx instead of aiohttp

--- nepse/core.py
+++ nepse/core.py
@@ -1,16 +1,27 @@
-from typing import Optional
+from typing import AsyncIterator, Optional
 
-import aiohttp
+import httpx
 
 from nepse.security.types import SecurityResponse
+from nepse.utils import ClientWrapperHTTPX
 
 from .security import SecurityClient
 
 
 class Client:
     def __init__(self) -> None:
-        self._session = aiohttp.ClientSession()
-        self._security_client = SecurityClient(self._session)
+        self._session = httpx.AsyncClient()
+        self._client_wrapper = ClientWrapperHTTPX(self._session)
+        self._security_client = SecurityClient(self._client_wrapper)
+        
+
+    async def companies(self) -> AsyncIterator[SecurityResponse]:
+        base_securities = self._security_client._securities_basic_cache.values()
+        if not base_securities:
+            await self._security_client.fetch_all_base_securities()
+
+        for security in base_securities:
+            yield await self._security_client.fetch_security_response(security.id)
 
     async def get_company(
         self, id: Optional[int] = None, symbol: Optional[str] = None
@@ -19,4 +30,4 @@ class Client:
         return model
 
     async def close(self) -> None:
-        await self._session.close()
+        await self._session.aclose()
