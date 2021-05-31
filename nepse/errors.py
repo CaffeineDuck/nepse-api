5294d744dfcec848c357cf7cafdc166d42e73c3b
Samrid Pandit <samrid.pandit@gmail.com>
2021-05-31 23:56:31 +0545

    Subclassed all the errors with 'NepseException'

--- nepse/errors.py
+++ nepse/errors.py
@@ -1,8 +1,16 @@
-class SymbolOrIdNotPassed(Exception):
+class NepseException(Exception):
+    def __init__(self) -> None:
+        self.stop_code = True
+
+    def __str__(self) -> str:
+        raise NotImplementedError()
+
+
+class SymbolOrIdNotPassed(NepseException):
     def __str__(self) -> str:
         return "Neither symbol nor ID was passed for fetching the security!"
 
 
-class NotFound(Exception):
+class NotFound(NepseException):
     def __str__(self) -> str:
         return "The given details was not found!"
