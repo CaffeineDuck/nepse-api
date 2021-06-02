c2388e6591c4cbef4846dc99ff44815d3cefd1d4
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-02 21:22:12 +0545

    Added a utils.get function

--- nepse/utils.py
+++ nepse/utils.py
@@ -1,7 +1,10 @@
-from json import JSONDecodeError
+from operator import attrgetter
+from typing import Any, Iterable, Optional, TypeVar
 
 import attr
 import httpx
+
+T = TypeVar("T")
 
 
 @attr.frozen
@@ -10,3 +13,25 @@ class ClientWrapperHTTPX:
 
     async def get_json(self, url) -> object:
         return (await self._client.get(url)).json()
+
+
+def get(iterable: Iterable[T], **attrs: Any) -> Optional[T]:
+    attrget = attrgetter
+
+    # Special case the single element call
+    if len(attrs) == 1:
+        k, v = attrs.popitem()
+        pred = attrget(k.replace("__", "."))
+        for elem in iterable:
+            if pred(elem) == v:
+                return elem
+        return None
+
+    converted = [
+        (attrget(attr.replace("__", ".")), value) for attr, value in attrs.items()
+    ]
+
+    for elem in iterable:
+        if all(pred(elem) == value for pred, value in converted):
+            return elem
+    return None
