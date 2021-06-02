de24fd780cf2f6c3a3f0f3ac905ee7e0de1daf05
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-02 19:34:40 +0545

    Now the Client accepts httpx.AsyncClient while initializing

--- basic_test.py
+++ basic_test.py
@@ -1,4 +1,6 @@
 from dataclasses import asdict
+from nepse.security.types import BaseSecurity
+from textwrap import wrap
 
 import httpx
 import humps
@@ -6,6 +8,15 @@ import pytest
 
 from nepse import Client
 from nepse.utils import ClientWrapperHTTPX
+
+
+@pytest.mark.asyncio
+async def test_all_companies():
+    async with httpx.AsyncClient() as client:
+        nepse_client = Client(httpx_client=client)
+        api_data = await nepse_client.security_client.get_companies()
+
+    assert api_data
 
 
 @pytest.mark.asyncio
@@ -18,7 +29,7 @@ async def test_right_security_data():
     api_data = humps.decamelize(data)
     await session.aclose()
 
-    nepse_client = Client(True, 10)
+    nepse_client = Client()
     wrapper_data = asdict(
         await nepse_client.security_client.get_company(symbol="UPPER")
     )
