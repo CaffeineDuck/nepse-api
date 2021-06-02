5b525952cedffc97bd3e18925289ec868edd751d
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-02 14:39:20 +0545

    Removed __version__ from __init__.py

--- nepse/__init__.py
+++ nepse/__init__.py
@@ -4,5 +4,4 @@ __package_name__ = "nepse-api"
 __package_description__ = "This is a API wrapper for NEPSE API."
 
 __author__ = "Samrid Pandit"
-__version__ = "0.4.1"
 __author_email__ = "samrid.pandit@gmail.com"
