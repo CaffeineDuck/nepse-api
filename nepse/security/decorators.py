4d62a5912c41a5d28617a5305c722002f803bc24
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-03 03:25:18 +0545

    Minor change according for the var name

--- nepse/security/decorators.py
+++ nepse/security/decorators.py
@@ -9,7 +9,7 @@ def securities_are_cached(func: _T) -> _T:
     @functools.wraps(func)
     async def predicate(self, *args, **kwargs):
         if not self._securities_basic_cache:
-            await self._update_basic_securities_cache()
+            await self._fetch_securities_cache()
         if iscoroutinefunction(func):
             return await func(self, *args, **kwargs)
         else:
