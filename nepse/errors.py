e971c11129c6d0e1968794a95a0ba591b6f68500
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-03 11:53:24 +0545

    Removed unused expection

--- nepse/errors.py
+++ nepse/errors.py
@@ -1,14 +1,10 @@
 class NepseException(Exception):
     def __init__(self) -> None:
-        self.stop_code = True
+        self._stop_code = True
 
     def __str__(self) -> str:
         raise NotImplementedError()
 
-
-class SymbolOrIdNotPassed(NepseException):
-    def __str__(self) -> str:
-        return "Neither symbol nor ID was passed for fetching the security!"
 
 
 class NotFound(NepseException):
