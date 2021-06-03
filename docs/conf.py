e071cc6abca43ab871d3122b1705148038392d7e
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-03 12:21:37 +0545

    Updated requirements.txt for docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Samrid Pandit"
 author = "Samrid Pandit"
 
 # The full version, including alpha/beta/rc tags
-release = "0.6.1"
+release = "0.6.2"
 
 
 # -- General configuration ---------------------------------------------------
