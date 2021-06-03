ca3022af9dadc73bf1aa57b1d3a5410269848136
Samrid Pandit <samrid.pandit@gmail.com>
2021-06-03 11:13:23 +0545

    Changed to rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.intersphinx",
     "sphinx.ext.extlinks",
-    # "sphinx_rtd_theme",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,9 +53,13 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
+# html_theme = "furo"
 
-html_conf = {"examples": "https://github.com/Samrid-Pandit/nepse-api/"}
+
+html_conf = {
+    'examples': "https://github.com/Samrid-Pandit/nepse-api/"
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
