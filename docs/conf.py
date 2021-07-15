672b939be3fa96cab162944d9e6f060d7b508c17
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-15 23:26:17 +0545

    [version-bump] Bumps to v1.1.10 in the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Samrid Pandit"
 author = "Samrid Pandit"
 
 # The full version, including alpha/beta/rc tags
-release = "1.1.1"
+release = "1.1.10"
 
 
 # -- General configuration ---------------------------------------------------
